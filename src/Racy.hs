d849f3242c4b09933a22db01f812dcb049533b7f
Nikita Tchayka <nick@booster.cloud>
2021-10-28 20:14:31 +0100

    Update

--- src/Racy.hs
+++ src/Racy.hs
@@ -1,7 +1,13 @@
 module Racy
   ( start,
+    compile,
   )
 where
 
+import qualified Data.EDN as EDN
+
 start :: IO ()
 start = putStrLn "Racy"
+
+compile :: [EDN.TaggedValue] -> [AHK.Statement]
+compile = undefined
